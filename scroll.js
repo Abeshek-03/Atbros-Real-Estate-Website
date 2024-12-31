61a425bcd2f592ea0caaa59e907c893289009ff8
Abeshek A <abeshek.a@gmail.com>
2024-12-31 22:01:04 +0530

    fixed navbar bug

--- scroll.js
+++ scroll.js
@@ -1,6 +1,4 @@
 function scrollToSection(event, sectionId) {
-    event.preventDefault(); // Prevent default anchor link behavior
-
     const element = document.getElementById(sectionId);
     const offset = -50; // Adjust this offset as needed (e.g., for fixed header)
 
